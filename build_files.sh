1bd0a615287353c536a105abacaa79cd257682a6
Ajmal Basheer <77728850+ajmalbuv@users.noreply.github.com>
2024-06-05 14:46:54 +0530

    ...

--- build_files.sh
+++ build_files.sh
@@ -2,10 +2,9 @@
 
 # Build the project
 echo "Building the project..."
-apt-get update 
 /usr/local/bin/pip3 install -r requirements.txt
 echo "Make Migration..."
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
