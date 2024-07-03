ce989ae11e4b54bd4ef2616136df2c5bc3c55201
Ajmal Basheer <77728850+ajmalbuv@users.noreply.github.com>
2024-07-03 19:40:23 +0530

    removed  /account/logout to /login in templates base.html,homepage.html,t_homepage.html

--- CollegeERP/settings.py
+++ CollegeERP/settings.py
@@ -70,8 +70,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'CollegeERP.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 # DATABASES = {
 #     'default': {
