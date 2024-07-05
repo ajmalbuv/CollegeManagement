4d8de955429e43582aa1062598d71bd795f30191
Ajmal Basheer <77728850+ajmalbuv@users.noreply.github.com>
2024-07-05 19:56:30 +0530

    login fixed for good i guess

--- CollegeERP/urls.py
+++ CollegeERP/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('accounts/login/',
          auth_views.LoginView.as_view(template_name='info/login.html'), name='login'),
     path('accounts/logout/',
-         auth_views.LogoutView.as_view(template_name='info/logout.html'), name='logout'),
+         auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
