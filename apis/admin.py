4f5ffb10428a3a02652dc756369514f60296290a
Ajmal Basheer <77728850+ajmalbuv@users.noreply.github.com>
2024-07-11 18:33:14 +0530

    Completely Unregistered Tokens and Groups

--- apis/admin.py
+++ apis/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from rest_framework.authtoken.models import TokenProxy
+from django.contrib.auth.models import Group
 
-# Register your models here.
+
+admin.site.unregister(Group)
+admin.site.unregister(TokenProxy)
