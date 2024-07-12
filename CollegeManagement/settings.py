33c3fb0425d6b9c9bb21e25731f7fe1e7a6d5918
Ajmal Basheer <77728850+ajmalbuv@users.noreply.github.com>
2024-07-12 10:19:29 +0530

    Installed Admin interface

--- CollegeManagement/settings.py
+++ CollegeManagement/settings.py
@@ -20,6 +20,8 @@ AUTH_USER_MODEL = "info.User"
 # Application definition
 
 INSTALLED_APPS = [
+    "admin_interface",
+    "colorfield",
     "info.apps.InfoConfig",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -122,9 +124,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
